64c41c8677acc3cc4a4de2f7374050be2023c48b
Leon Rosenberg <leon.rosenberg@anotheria.net>
2013-11-17 22:01:11 +0100

    finalizing

--- src/main/java/de/zaunberg/burgershop/ui/OrderController.java
+++ src/main/java/de/zaunberg/burgershop/ui/OrderController.java
@@ -2,6 +2,8 @@ package de.zaunberg.burgershop.ui;
 
 import de.zaunberg.burgershop.service.Order;
 import de.zaunberg.burgershop.service.ShopService;
+import de.zaunberg.burgershop.service.ShopableItem;
+import net.anotheria.util.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.LinkedList;
 
 /**
  * TODO comment this class
@@ -26,15 +28,22 @@ public class OrderController {
 	private static Logger log = LoggerFactory.getLogger(OrderController.class);
 
 	@RequestMapping(value = "/order.html")
-	public String order(HttpServletRequest request, HttpServletResponse response, @RequestParam()String choice1, @RequestParam String choice2, @RequestParam String choice3){
+	public String order(HttpServletRequest request, @RequestParam()String choice1, @RequestParam String choice2, @RequestParam String choice3){
 
 		log.debug("Incoming order "+choice1+", "+choice2+", "+choice3);
 		Order order = service.placeOrder(choice1, choice2, choice3);
 
 		log.debug("Placed order "+order);
 
+		LinkedList<String> orderedItems = new LinkedList<String>();
+		for (ShopableItem item : order.getItems()){
+			orderedItems.add(item.getName());
+		}
+		request.setAttribute("ordereditems", orderedItems);
+		request.setAttribute("totalPrice", NumberUtils.currencyFormat((double)order.getTotalPrice()/100, ','));
 
-		return null;
+
+		return "confirmation";
 	}
 
 }
