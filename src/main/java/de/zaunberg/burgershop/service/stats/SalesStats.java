08273dfc3c5bda93367b7a8187235203286fb786
Leon Rosenberg <leon.rosenberg@anotheria.net>
2013-12-12 15:40:55 +0100

    added SalesStats - the stats object

---
+++ src/main/java/de/zaunberg/burgershop/service/stats/SalesStats.java
@@ -0,0 +1,55 @@
+package de.zaunberg.burgershop.service.stats;
+
+import net.anotheria.moskito.core.predefined.Constants;
+import net.anotheria.moskito.core.producers.AbstractStats;
+import net.anotheria.moskito.core.stats.StatValue;
+import net.anotheria.moskito.core.stats.TimeUnit;
+import net.anotheria.moskito.core.stats.impl.StatValueFactory;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 12.12.13 13:01
+ */
+public class SalesStats extends AbstractStats{
+
+
+	/**
+	 * The number of sales.
+	 */
+	private StatValue number;
+
+	/**
+	 * The volume of sales.
+	 */
+	private StatValue volume;
+
+	public SalesStats(String name) {
+		super(name);
+
+		number = StatValueFactory.createStatValue(Long.valueOf(0), "number", Constants.getDefaultIntervals());
+		volume = StatValueFactory.createStatValue(Long.valueOf(0), "volume", Constants.getDefaultIntervals());
+	}
+
+	public void addSale(int priceInCents){
+		number.increase();
+		volume.increaseByInt(priceInCents);
+	}
+
+	public long getNumber(String intervalName){
+		return number.getValueAsLong(intervalName);
+	}
+
+	public long getVolume(String intervalName){
+		return volume.getValueAsLong(intervalName);
+	}
+
+
+	@Override
+	public String toStatsString(String s, TimeUnit timeUnit) {
+		return null;
+	}
+
+
+}
