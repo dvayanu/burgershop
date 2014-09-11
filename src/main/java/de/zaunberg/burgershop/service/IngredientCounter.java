546dc6f0d664d21299ca9116915389e77bb4eab6
Leon Rosenberg <leon.rosenberg@anotheria.net>
2014-09-12 00:12:01 +0200

    added counter from step2

---
+++ src/main/java/de/zaunberg/burgershop/service/IngredientCounter.java
@@ -0,0 +1,10 @@
+package de.zaunberg.burgershop.service;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 12.09.14 00:03
+ */
+public class IngredientCounter {
+}
---
+++ src/main/java/de/zaunberg/burgershop/service/OrderCounter.java
@@ -0,0 +1,10 @@
+package de.zaunberg.burgershop.service;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 12.09.14 00:02
+ */
+public class OrderCounter {
+}
