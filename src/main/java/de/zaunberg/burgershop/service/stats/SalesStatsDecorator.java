22677c620b3a4d80e27815ab9496fed69a23ddc2
Leon Rosenberg <leon.rosenberg@anotheria.net>
2015-03-26 23:55:38 +0100

    adoptations to 2.5.7

---
+++ src/main/java/de/zaunberg/burgershop/service/stats/SalesStatsDecorator.java
@@ -0,0 +1,10 @@
+package de.zaunberg.burgershop.service.stats;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 12.09.14 00:16
+ */
+public class SalesStatsDecorator {
+}
---
+++ src/main/java/de/zaunberg/burgershop/service/stats/SalesStatsFactory.java
@@ -0,0 +1,10 @@
+package de.zaunberg.burgershop.service.stats;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 12.09.14 00:15
+ */
+public class SalesStatsFactory {
+}
